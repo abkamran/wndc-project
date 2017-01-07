e083e42c1a24a2a43b17ac98dfc65fbeb50c185d
Kamran AB Syed <1205abkamran@gmail.com>
2017-01-08 02:30:42 +0530

    js files

---
+++ wndc-header.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class WNdCHeader extends Component {
+   
+   render() {
+       return <h1>Welcome to WNdC, the one place for all NEWS!</h1>
+   }
+}
